827cdcc92b1ef07fa13f555c3bd731e3dccc7cce
Shahram M. Hassan <shahram.mebashar@gmail.com>
2021-09-24 23:51:54 +0300

    update phone number

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -83,7 +83,7 @@ class Navbar extends Component {
                 <Icon size="xs">
                   <img src={PhoneSvgIcon} alt="" />
                 </Icon>
-                +964-751 4104314 (EN)
+                +964 (0) 750 076 9538 (EN)
               </div>
             </div>
             <HumbergerMenu
